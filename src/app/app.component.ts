13b57494c575af1cee7a08864976303ff08251d4
Wilson Junior <wilsonjunior@sobral.ce.gov.br>
2020-02-08 19:52:52 -0300

    change layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {ButtonModule} from 'primeng/button';
 import {ToolbarModule} from 'primeng/toolbar';
-
+import {InputTextModule} from 'primeng/inputtext';
 import {NbaServiceService} from './service/nba-service.service';
 import {CardModule} from 'primeng/card';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ButtonModule} from 'primeng/button';
 import {CardModule} from 'primeng/card';
+import {InputTextModule} from 'primeng/inputtext';
 import {ToolbarModule} from 'primeng/toolbar';
+import {ProgressSpinnerModule} from 'primeng/progressspinner';
 
 import { HttpClientModule } from '@angular/common/http';
 
@@ -21,6 +23,8 @@ import { HttpClientModule } from '@angular/common/http';
     ButtonModule,
     CardModule,
     ToolbarModule,
+    ProgressSpinnerModule,
+    InputTextModule,
     HttpClientModule,
     BrowserAnimationsModule,
   ],
